9d9856b220983a67716c99109bae6088dcf59291
Mathews Thomas <mathews@Mathewss-MacBook-Pro.local>
2022-08-05 15:25:15 +0100

    Added comment

--- microblog.py
+++ microblog.py
@@ -6,6 +6,6 @@ from app.models import User, Posts
 def make_shell_context():
     return { 'db' : db, 'User': User, 'Posts': Posts}
 
-
+#main
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
